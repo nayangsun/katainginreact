2bbab41d07844e6c2faf425956a07fc20001b899
nayangsun <nyangsun@gmail.com>
2023-12-18 16:38:54 +0900

    fix

--- src/lib/playground.js
+++ src/lib/playground.js
@@ -1,23 +1,38 @@
+// const fs = require("fs");
+// const path = require("path");
+
 export function runCode() {
-  my_test(function ({ matchComponents, theme }) {
-    a(
-      {
-        a: (test) => {
-          console.log(test + " a type");
-          b("b test");
+  my_plugin(function ({ matchComponents, theme }) {
+  //   let iconsDir = path.join(__dirname, "./vendor/heroicons/optimized")
+  //   let values = {}
+  //   let icons = [
+  //     ["", "/24/outline"],
+  //     ["-solid", "/24/solid"],
+  //     ["-mini", "/20/solid"]
+  //   ]
+  //   icons.forEach(([suffix, dir]) => {
+  //     fs.readdirSync(path.join(iconsDir, dir)).map(file => {
+  //       let name = path.basename(file, ".svg") + suffix
+  //       values[name] = { name, fullPath: path.join(iconsDir, dir, file) }
+  //     })
+  //   })
+    
+    matchComponents({
+        "type": (text) => {
+          console.log(text + " a type")
+          theme("b theme")
         },
-      },
-      "btypes"
-    );
-  });
+      }, "b types")
+  })
 }
 
-function my_test(a, b) {
-  a({
-    a: (callback) => {
-      callback.a("my");
+function my_plugin(components, options) {
+  components({
+    matchComponents: (callback, text) => {
+      callback.type("my");
+      callback.type(text);
     },
-    b: function (obj) {
+    theme: function (obj) {
       console.log(obj);
     },
   });
