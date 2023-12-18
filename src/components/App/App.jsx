3a08e2180895ea7375e88c4a8287411c5f03d851
nayangsun <nyangsun@gmail.com>
2023-12-18 14:23:50 +0900

    feat: add prettier

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from '../../assets/react.svg'
-import viteLogo from '../../../public/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "../../assets/react.svg";
+import viteLogo from "../../../public/vite.svg";
+import "./App.css";
+import { runCode } from "../../lib/playground";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  runCode();
 
   return (
     <>
@@ -29,7 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
