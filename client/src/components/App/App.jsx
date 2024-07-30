6b76b04d22830606435573bf9cba6f477f3a0165
nayangsun <nyangsun@gmail.com>
2024-07-30 11:22:51 +0900

    up

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,33 +1,29 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { StyledEngineProvider } from '@mui/material/styles';
-import { ThemeProvider } from '@mui/material/styles';
 import { SnackbarProvider } from 'notistack';
 import { ConfirmProvider } from 'material-ui-confirm';
 import CssBaseline from '@mui/material/CssBaseline';
-import Navigation from './Navigation';
-import client from './apolloClient'; // Remove this line if you don't need Apollo anymore
+import Navigation from '../Navigation/Navigation';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <StyledEngineProvider injectFirst>
-          <ThemeProvider>
-            <SnackbarProvider
-              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-            >
-              <ConfirmProvider>
-                <CssBaseline />
-                <Navigation />
-              </ConfirmProvider>
-            </SnackbarProvider>
-          </ThemeProvider>
+          <SnackbarProvider
+            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          >
+            <ConfirmProvider>
+              <CssBaseline />
+              <Navigation />
+            </ConfirmProvider>
+          </SnackbarProvider>
         </StyledEngineProvider>
       </QueryClientProvider>
-    </Router>
+    </BrowserRouter>
   );
 }
 
