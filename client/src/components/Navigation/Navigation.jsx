515aa7276797f0a155d80a745959e1ff2eec84b3
나양선 <nyangsun@gmail.com>
2024-07-30 11:26:01 +0900

    feat: add QueryClient and BrowserRouter (#3)
    * wip
    
    * up

---
+++ client/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../Home/Home";
+
+
+function Navigation() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default Navigation;
