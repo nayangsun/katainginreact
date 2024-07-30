51ccbdb8723cb045cfd6bb84d8d8d1d31b8adb5d
nayangsun <nyangsun@gmail.com>
2024-07-30 11:58:06 +0900

    format

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,10 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
-import { StyledEngineProvider } from '@mui/material/styles';
-import { SnackbarProvider } from 'notistack';
-import { ConfirmProvider } from 'material-ui-confirm';
-import CssBaseline from '@mui/material/CssBaseline';
-import Navigation from '../Navigation/Navigation';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+import { StyledEngineProvider } from "@mui/material/styles";
+import { SnackbarProvider } from "notistack";
+import { ConfirmProvider } from "material-ui-confirm";
+import CssBaseline from "@mui/material/CssBaseline";
+import Navigation from "../Navigation/Navigation";
 
 const queryClient = new QueryClient();
 
--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../Home/Home";
 
-
 function Navigation() {
   return (
     <Routes>
