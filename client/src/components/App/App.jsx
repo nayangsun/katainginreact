e4ff138d609e92ea8a8a44481cc21a8d1457b5ed
nayangsun <nyangsun@gmail.com>
2024-08-02 15:23:11 +0900

    refactor: restructure App.jsx component hierarchy

---
+++ client/src/components/App/App.jsx
@@ -0,0 +1,30 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter } from 'react-router-dom';
+import { StyledEngineProvider } from '@mui/material/styles';
+import { SnackbarProvider } from 'notistack';
+import { ConfirmProvider } from 'material-ui-confirm';
+import CssBaseline from '@mui/material/CssBaseline';
+import Navigation from '../Navigation/Navigation';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <StyledEngineProvider injectFirst>
+          <SnackbarProvider
+            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          >
+            <ConfirmProvider>
+              <CssBaseline />
+              <Navigation />
+            </ConfirmProvider>
+          </SnackbarProvider>
+        </StyledEngineProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
