6a937652cdb20d0434cf492eecbf82de282d66e1
나양선 <nyangsun@gmail.com>
2024-08-20 01:08:00 +0900

    wip

---
+++ client/eslint.config.cjs
@@ -0,0 +1,47 @@
+const globals = require("globals");
+const reactPlugin = require("eslint-plugin-react");
+const reactHooksPlugin = require("eslint-plugin-react-hooks");
+const reactRefreshPlugin = require("eslint-plugin-react-refresh");
+const jest = require('eslint-plugin-jest');
+
+module.exports = [
+  ...require("@eslint/js").configs.recommended,
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      },
+      parserOptions: {
+        ...reactPlugin.configs.recommended.parserOptions,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    ignores: ["dist", "eslint.config.mjs"],
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "react-refresh": reactRefreshPlugin,
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/prop-types': 'off',
+      'react/no-unescaped-entities': 'off',
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    files: ['test/**'],
+    ...jest.configs['flat/recommended'],
+  },
+];
--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { React, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { formatSentence } from "../../lib/utils";
 import { Link, Box, Typography, Container } from "@mui/material";
