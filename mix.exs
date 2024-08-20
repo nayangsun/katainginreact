8b6104078251c8480bd46f5d2ab64b5cce992ccc
nayangsun <nyangsun@gmail.com>
2024-08-20 12:03:27 +0900

    bump Erlang and Elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ReactKata.MixProject do
     [
       app: :react_kata,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
