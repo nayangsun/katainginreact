9c467c49bfd753fd22c3d39b052adc64c95f58c8
나양선 <nyangsun@gmail.com>
2024-08-29 15:25:18 +0900

    Add fetch_json utility with custom ResponseError handling

---
+++ client/src/lib/fetch_json.js
@@ -0,0 +1,17 @@
+class ResponseError extends Error {
+  constructor(name, message) {
+    super(message);
+    this.name = name;
+  }
+}
+
+export default function fetchJson(url, options = {}) {
+  return fetch(url, { ...options, credentials: "include" }).then((response) => {
+    if (!response.ok) {
+      return response.json().then((data) => {
+        throw new ResponseError(response.statusText, data.message);
+      });
+    }
+    return response.json();
+  });
+}
