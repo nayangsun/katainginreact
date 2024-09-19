b9321582f3da5d60471122db12f2c2cb587e1243
nayangsun <nyangsun@gmail.com>
2024-09-19 15:18:38 +0900

    style: update styles for NewsResourceCard component

--- client/src/components/NewsResourceCard/NewsResourceCard.jsx
+++ client/src/components/NewsResourceCard/NewsResourceCard.jsx
@@ -7,17 +7,12 @@ import {
   Box,
   IconButton,
   Chip,
+  CardActionArea,
+  CardActions,
 } from "@mui/material";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import { styled } from "@mui/system";
-
-const StyledCard = styled(Card)({
-  borderRadius: 16, // theme.shape.borderRadius * 2
-  padding: 16, // theme.spacing(2)
-  boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.16)", // theme.shadows[3]
-  width: "100%",
-});
 
 const NotificationDot = styled(Box)({
   width: 8,
@@ -26,7 +21,6 @@ const NotificationDot = styled(Box)({
   backgroundColor: "#f50057", // theme.palette.secondary.main
   marginRight: 8, // theme.spacing(1)
 });
-
 function NewsResourceCard({
   newsResource,
   isBookmarked,
@@ -36,48 +30,66 @@ function NewsResourceCard({
   onTopicClick,
 }) {
   return (
-    <StyledCard onClick={onClick}>
-      {newsResource.headerImageUrl && (
-        <CardMedia
-          component="img"
-          height="180"
-          image={newsResource.headerImageUrl}
-          alt="header image"
-        />
-      )}
-      <CardContent>
-        <Box display="flex" justifyContent="space-between" alignItems="center">
-          <Typography variant="h6">{newsResource.title}</Typography>
+    <Card
+      sx={{
+        borderRadius: 4,
+        width: "100%",
+      }}
+    >
+      <CardActionArea onClick={onClick}>
+        {newsResource.headerImageUrl && (
+          <CardMedia
+            component="img"
+            height="180"
+            image={newsResource.headerImageUrl}
+            alt="header image"
+          />
+        )}
+        <CardContent>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Typography variant="h6">{newsResource.title}</Typography>
+          </Box>
+
+          <Box display="flex" alignItems="center" mt={1}>
+            {!hasBeenViewed && <NotificationDot />}
+            <Typography variant="body2" color="textSecondary">
+              {new Date(newsResource.publishDate).toLocaleDateString()} •{" "}
+              {newsResource.type}
+            </Typography>
+          </Box>
+
+          <Typography variant="body1" mt={2}>
+            {newsResource.content}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          width="100%"
+        >
+          <Box display="flex" gap={1} overflow="auto">
+            {newsResource.topics.map((topic) => (
+              <Chip
+                key={topic.id}
+                label={topic.name.toUpperCase()}
+                clickable
+                onClick={() => onTopicClick(topic.id)}
+              />
+            ))}
+          </Box>
           <IconButton onClick={onToggleBookmark}>
             {isBookmarked ? <BookmarkIcon /> : <BookmarkBorderIcon />}
           </IconButton>
         </Box>
-
-        <Box display="flex" alignItems="center" mt={1}>
-          {!hasBeenViewed && <NotificationDot />}
-          <Typography variant="body2" color="textSecondary">
-            {new Date(newsResource.publishDate).toLocaleDateString()} •{" "}
-            {newsResource.type}
-          </Typography>
-        </Box>
-
-        <Typography variant="body1" mt={2}>
-          {newsResource.content}
-        </Typography>
-
-        <Box display="flex" gap={1} mt={2} overflow="auto">
-          {newsResource.topics.map((topic) => (
-            <Chip
-              key={topic.id}
-              label={topic.name.toUpperCase()}
-              clickable
-              onClick={() => onTopicClick(topic.topic.id)}
-              // color={topic.isFollowed ? 'primary' : 'default'}
-            />
-          ))}
-        </Box>
-      </CardContent>
-    </StyledCard>
+      </CardActions>
+    </Card>
   );
 }
 
